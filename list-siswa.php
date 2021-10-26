a7d81fe0e49cbc9c0cc00cbb670964b756a92b33
agunwiguna <wgnagun@gmail.com>
2021-10-26 20:43:34 +0700

    initial commit

---
+++ list-siswa.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Siswa</title>
+</head>
+<body>
+    <h2>Data Siswa</h2>
+    <br/>
+    <table border="1">
+        <thead>
+            <tr>
+                <td>No.</td>
+                <td>NIS</td>
+                <td>Nama</td>
+                <td>Jenis Kelamin</td>
+                <td>Tanggal Lahir</td>
+                <td>Alamat</td>
+                <td>Kontak</td>
+                <td>Aksi</td>
+            </tr>            
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
